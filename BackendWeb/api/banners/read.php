b80ba15e6281a8acbba69a7d1634b6866333b132
Trần Trọng Phúc <trongphuc636@gmail.com>
2026-03-12 14:40:11 +0700

    feat: add CORS headers to banners and products read endpoints

--- BackendWeb/api/banners/read.php
+++ BackendWeb/api/banners/read.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include_once dirname(__DIR__, 2) . '/config/database.php';
 include_once dirname(__DIR__, 2) . '/models/Banner.php';
